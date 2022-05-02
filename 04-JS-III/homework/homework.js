b84be54b9d1a98cd846a2a70dfa3418b5a5d9053
Your Name <brundua99@gmail.com>
2022-05-02 20:17:30 -0300

    Homeworks

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,22 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  console.log(array[0])
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  var ultimo = array.length-1
+  console.log(array[ultimo]) //VERRRRR
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
@@ -23,14 +27,22 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let i=0; i<array.length;i++){
+    
+    console.log(array[i]+1)
+  }
 }
+incrementarPorUno([1,2,3,4])
 
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento)
+  console.log(array)
 }
+
 
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -38,6 +50,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  console.log(array)
 }
 
 
@@ -47,6 +61,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  for(let i=0;i<palabras.length;i++){
+  
 }
 
 
